b3cafab93cb95215c4b8664f9ff5593e6a1334ae
ChrisCox12 <chriscox9856@gmail.com>
2022-01-17 20:57:34 -0500

    got reset working for when there's not a match; displays and hides flipped items

--- src/components/GamePiece/GamePiece.js
+++ src/components/GamePiece/GamePiece.js
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './GamePiece.css';
 
-export default function GamePiece() {
+export default function GamePiece({ 
+    value, index, firstMove, setFirstMove, 
+    secondMove, setSecondMove, assignMove, flippedPieces, setFlippedPieces,
+    unAssignMove, reset, setReset }) {
+    const [pieceValue, setPieceValue] = useState('');
+    const [pieceIndex, setPieceIndex] = useState(-1);
+    const [flipped, setFlipped] = useState(false);
+    const [pieceMatched, setPieceMatched] = useState(false);
+
+    useEffect(() => {
+        setPieceValue(value);
+        setPieceIndex(index);
+        /* console.log(`piece value: ${value}`); */
+        /* console.log(`value: ${value}, index: ${index}`) */
+    }, [value, index]);
+
+
+    useEffect(() => {
+        const toFlip = document.getElementById(index);
+
+
+        if (flipped) {
+            /* console.log('toFlip: ', toFlip);
+            console.log('flipped'); */
+
+            toFlip.classList.remove('not-flipped');
+            toFlip.classList.add('flipped');
+        }
+        else {
+            if(toFlip.classList.contains('flipped')){
+                /* console.log('contains') */
+                toFlip.classList.remove('flipped');
+                toFlip.classList.add('not-flipped');    
+            }
+        }
+
+    }, [flipped, pieceIndex, index]);
+
+    useEffect(() => {
+        /* const toReset = document.querySelectorAll('.flipped'); */
+        if(reset) {
+            setFlipped(false);
+            setReset(false);
+        }
+    }, [reset, setReset]);
+
+    /* useEffect(() => {
+        const f = document.querySelectorAll('.GamePiece__piece--flipped');
+        
+        if(f.length === 2){
+            console.log('two pieces')
+            console.log(f[0], f[1])
+        }
+    }, [flipped, pieceIndex]); */
+
+
+    function flipPiece() {
+        setFlipped(!flipped);
+        /* console.log('flipped: ', flipped) */
+        /* console.log(`card clicked: ${e.target}`);
+        console.log(e.target); */
+
+        /* assignMoves(); */
+        assignMove({
+            value: pieceValue,
+            index: pieceIndex,
+        });
+    }
+
+    /* function assignMoves() {
+        if(firstMove === null){
+            console.log('null first move')
+            setFirstMove({
+                value: pieceValue,
+                pieceIndex: pieceIndex
+            });
+        }
+        else{
+            console.log('first move not null')
+            setSecondMove({
+                value: pieceValue,
+                pieceIndex: pieceIndex
+            });
+        }
+    } */
+
+    
+
     return (
-        <div>
-            
+        <div className="GamePiece not-flipped" onClick={flipPiece} id={index}>
+            {/* flipped ? 
+                <div className="GamePiece__piece--flipped">{pieceValue}</div> 
+                : <div className="GamePiece__piece--not-flipped"></div> */
+            }
+            <div className="GamePiece__content">
+                {flipped ? 
+                    pieceValue 
+                    : null
+                }
+            </div>
         </div>
     )
 }
--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -7,9 +7,63 @@ export default function Gameboard({ theme, numPlayers, gridSize }) {
     const [moves, setMoves] =  useState(0);
     const [showMenu, setShowMenu] = useState(false);
     const [gameState, setGameState] = useState([]);
+    const [firstMove, setFirstMove] = useState({ value: -1, index: -1});
+    const [secondMove, setSecondMove] = useState({value: -1, index: -1});
+    const [reset, setReset] = useState(false);
+    const [flippedPieces, setFlippedPieces] = useState(0);
     const [boardPieces, setBoardPieces] = useState(
         generateBoardPieces()
     );
+
+    useEffect(() => {
+        /* console.log(`effect: ${boardPieces}`) */
+        let state = [];
+        boardPieces.forEach(piece => {
+            state.push({
+                value: piece,
+                flipped: false,
+                matched: false
+            });
+        });
+        /* console.log(`state: ${JSON.stringify(state)}`) */
+        setGameState(state);
+        /* console.log(`Game state: ${JSON.stringify(state)}`) */
+    }, [boardPieces]);
+
+    useEffect(() => {
+        /* const f = document.querySelectorAll('.GamePiece__piece--flipped'); */
+        const f = document.querySelectorAll('.flipped');
+        
+        if(f.length === 2){
+            console.log('two pieces');
+            console.log(f[0], f[1]);
+        }
+
+        if(firstMove.index !== -1 && secondMove.index !== -1) {
+            if(firstMove.value === secondMove.value) {
+                console.log('match');
+            }
+            else {
+                console.log('no match');
+                setTimeout(() => {
+                    f.forEach(item => {
+                        item.classList.remove('flipped');
+                        item.classList.add('not-flipped');
+                        setReset(true);
+                    })
+                }, 1500);
+                
+                /* f.forEach(item => {
+                    console.log(item)
+                    item.classList.remove('flipped');
+                    item.classList.add('not-flipped')
+                    setReset(true);
+                }); */
+            }
+        }
+        /* console.log(f[0]) */
+
+    }, [firstMove, secondMove]);
 
     function generateBoardPieces() {
         if(gridSize === 4) {
@@ -35,6 +89,9 @@ export default function Gameboard({ theme, numPlayers, gridSize }) {
             }
 
             toReturn = [...nums, ...nums];
+
+            // for randomizing the array
+            toReturn.sort(() => Math.random() - 0.5);
         }
 
         /* console.log(`toReturn: ${toReturn}`) */
@@ -64,6 +121,59 @@ export default function Gameboard({ theme, numPlayers, gridSize }) {
 
     function toggleOverlay() {
         setShowMenu(!showMenu);
+    }
+
+    function assignMove(move) {
+        console.log('firstMove: ', firstMove);
+        console.log('move: ', move)
+        
+        if(firstMove.index === move.index) {
+            setFirstMove({value: -1, index: -1});
+        }
+        else{
+            if(firstMove.index === -1) {
+                setFirstMove(move);
+            }
+            else {
+                setSecondMove(move);
+            }
+        }
+
+        /* if(firstMove.value === -1) {
+            setFirstMove(move);
+            console.log('first move: ' , move);
+        }
+        else if(firstMove !== null && secondMove === null) {
+            setSecondMove(move);
+            console.log('second move: ', move);
+        } */
+        
+        
+
+        /* checkForMatch(); */
+    }
+
+    function unAssignMove(move) {
+        if(firstMove === move) {
+            setFirstMove(null);
+        }
+    }
+
+    function checkForMatch() {
+        console.log('checking');
+
+        if(firstMove.index !== -1 && secondMove.index !== -1) {
+            if(firstMove.value === secondMove.value) {
+                console.log('match');
+            }
+            else {
+                console.log('no match');
+            }
+        }
+        /* console.log(`flipped pieces: ${flippedPieces}`) */
+        /* if(flippedPieces.length === 2) {
+
+        } */
     }
 
     return (
@@ -106,13 +216,36 @@ export default function Gameboard({ theme, numPlayers, gridSize }) {
 
             {gridSize === 4 && 
                 <div className='Gameboard__board board--4x4'>
-                    
+                    {boardPieces.map((piece, index) => {
+                        return (
+                            <GamePiece 
+                                value={piece} 
+                                key={index} 
+                                index={index}
+                                firstMove={firstMove}
+                                setFirstMove={setFirstMove}
+                                secondMove={secondMove}
+                                setSecondMove={setSecondMove}
+                                assignMove={assignMove}
+                                flippedPieces={flippedPieces}
+                                setFlippedPieces={setFlippedPieces}
+                                unAssignMove={unAssignMove}
+                                reset={reset}
+                                setReset={setReset}
+                            />
+                        );
+                    })}
                 </div>
             }
 
             {gridSize === 6 && 
                 <div>6x6 grid</div>
             }
+
+            <button onClick={() => console.log(firstMove)}>First Move</button>
+            <button onClick={() => console.log(secondMove)}>Second Move</button>
+            <button onClick={() => console.log(flippedPieces)}>Flipped Pieces</button>
+            <button onClick={() => console.log(reset)}>Reset</button>
         </div>
     )
 }
